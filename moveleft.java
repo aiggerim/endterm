21ff9b25b5f99beb089b3d5e386f2d98ca6c7160
aiggerim <162737141+aiggerim@users.noreply.github.com>
2025-05-08 14:40:00 +0500

    Add moveleft.java (Concrete strategy to move left)

--- moveleft.java
+++ moveleft.java
@@ -3,6 +3,6 @@ package maria;
 public class moveleft implements movestrategy {
     @Override
     public void move(mario mario) {
-        mario.leftMove(); // ✅ Дәл осылай жазу керек
+        mario.leftMove();
     }
 }
